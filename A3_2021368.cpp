487456a5fbb914b7437a1eedd42a5be8c6b762f0
Faizan Haq <100567940+fizo-neechan@users.noreply.github.com>
2022-03-26 13:45:46 +0500

    Update A3_2021368.cpp
    - 2x2 array
    - problems:
    Printing, CLS not working properly
    
    - No mouse click

--- A3_2021368.cpp
+++ A3_2021368.cpp
@@ -1,16 +1,53 @@
 #include <iostream>
-#include <random>
-#include <Windows.h>
-#include <time.h>
-#include <stdio.h>
 #include <math.h>
 
+#define WIDTH 80
+#define HEIGHT 80
+#define FPS 30
+#define PI 3.14
+
+#define R1 1
+#define R2 2
+
+class Screen{
+    char surface[WIDTH][HEIGHT];
+
+    public:
+        Screen(){
+            // system("CLS"); //Flush screen
+            clearDisplay();
+        }
+
+        void putChar(int x, int y, char a){
+            surface[x][y] = a;
+        }   
+        
+        void flip(){
+            std::cout << "\x1b[H";
+            for(int i = 0; i < HEIGHT; i++){
+                for(int j = 0; j < WIDTH; j++){
+                    std::cout << surface[i][j];
+                }
+                std::cout << std::endl;
+            }
+            // system("CLS");
+
+            clearDisplay();
+        }
+
+        void clearDisplay(){
+            for(int i = 0; i < HEIGHT; i++){
+                for(int j = 0; j < WIDTH; j++){
+                    surface[i][j] = ' ';
+                }
+            }
+        }
+};
 
 class Donut{
-    private:
-        float A, B;
-        float z[1760];
-        char b[1760];
+    Screen display;
+    float A, B, theta, phi;
+    float z_buffer[WIDTH][HEIGHT];
 
     public:
         Donut(){
@@ -18,60 +55,69 @@ class Donut{
             B = 0;
         }
 
-        void print(){
-            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-            SetConsoleTextAttribute(hConsole , 252);
-            std::cout << "\x1b[H";
-            for (int k = 0; k < 1761; k++)
-            { 
-                std::cout << (char)(k % 80 ? b[k] : 10);
-            }
+        void update(float vel){
+            A += vel;     //increase x rotation speed
+            B += vel;  //increase z rotation speed
+
+            clearBuffer();
         }
 
-        void Update(){
+        void clearBuffer(){
+            for(int i = 0; i < HEIGHT; i++)
+                for(int j = 0; j < WIDTH; j++)
+                    z_buffer[i][j] = 0;
+        }
+
+        void drawDonut(){
             std::cout << "\x1b[2J";
-            while (true)
-            {
-                memset(b, 32, 1760);
-                memset(z, 0, 7040);
-                for (float theta = 0; theta < 6.28; theta += 0.07)
-                {
-                    for (float phi = 0; phi < 6.28; phi += 0.02)
-                    {
-                        float c = sin(phi); // sin(phi)
-                        float d = cos(theta); // cos(theta)
-                        float e = sin(A); // sin(A)
-                        float f = sin(theta); // sin(theta)
-                        float g = cos(A); // cos(A)
-                        float h = d + 2;  // (R2 + R1cos(theta)) //R2 is taken as 2 here
-                        float D = 1 / (c * h * e + f * g + 5); // 1/(z + K2) //K2 is taken as 5
-                        float l = cos(phi); // cos(phi)
-                        float m = cos(B); // cos(B)
-                        float n = sin(B); // sin(B)
-                        float t = c * h * g - f * e;
-                        int x = 40 + 30 * D * (l * h * m - t * n);
-                        int y = 12 + 15 * D * (l * h * n + t * m);
-                        int o = x + 80 * y;
-                        int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-                        if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o])
-                        {
-                            z[o] = D;
-                            b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
-                        }
+
+            for (float theta = 0; theta < 6.28; theta += 0.07){
+                for (float phi = 0; phi < 6.28; phi += 0.02){
+
+                    float sintheta = sin(theta); 
+                    float costheta = cos(theta); 
+
+                    float sinphi = sin(phi); 
+                    float cosphi = cos(phi); 
+                    
+                    float sinA = sin(A); 
+                    float cosA = cos(A); 
+                    
+                    float sinB = sin(B); 
+                    float cosB = cos(B); 
+                    
+                    float shiftedCircle = R1*costheta + R2;  // (R2 + R1cos(theta))
+
+                    float z = sinphi * shiftedCircle * sinA + sintheta * cosA;
+                    
+                    float ooz = 1 / (z + 5); // 1/(z + K2)   K2 = 5
+                    
+                    // K1 is the stretch factor -- the x stretch is half of y stretch to make the perfect donut shape
+                    int x = WIDTH/2 + 25 * ooz * (cosphi * shiftedCircle * cosB - (sinphi * shiftedCircle * cosA - sintheta * sinA) * sinB); //K1 = 25 here
+                    int y = HEIGHT/2 + 50 * ooz * (cosphi * shiftedCircle * sinB + (sinphi * shiftedCircle * cosA - sintheta * sinA)* cosB); // K1 = 50 here
+
+                    int luminance = 8 * ((sintheta * sinA - sinphi * costheta * cosA) * cosB - sinphi * costheta * sinA - sintheta * cosA - cosphi * costheta * sinB);
+                   
+                    if (HEIGHT > y && y > 0 && WIDTH > x && x > 0 && ooz > z_buffer[x][y]){
+                        z_buffer[x][y] = ooz;
+                        char a = ".,-~:;=!*#$@"[luminance > 0 ? luminance : 0];
+                        display.putChar(x, y, a);
                     }
                 }
-                A += 0.0704;
-                B += 0.0352;
-                print();
             }
+            display.flip();
         }
-
-        
-
 };
 
 int main(){
-    Donut D;
-    D.Update();
+    Donut donut;
+    
+// MAIN LOOP
+    while(1){
+        donut.drawDonut();
+        donut.update(0.5);
+
+        _sleep(1000/FPS);
+    }
     return 0;
 }
