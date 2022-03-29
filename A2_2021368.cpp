7b0016a4e5e996d39cfc3cd024a6ce7560b83bc6
Faizan Haq <100567940+fizo-neechan@users.noreply.github.com>
2022-03-29 19:18:28 +0500

    Update A2_2021368.cpp
    added PROPER comments

--- A2_2021368.cpp
+++ A2_2021368.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @brief copies char array s2 to s1
+ * 
+ * @param s1 destination
+ * @param s2 string to copy
+ * @return char* returns s1
+ */
 char* Mystrcpy(char* s1, const char* s2) {
     int i;
     for (i = 0; s2[i] != '\0'; i++) {
@@ -10,6 +17,14 @@ char* Mystrcpy(char* s1, const char* s2) {
     return s1;
 }
 
+/**
+ * @brief copies s2 to s1 till n number of characters
+ * 
+ * @param s1 destination
+ * @param s2 string to copy
+ * @param n number of characters
+ * @return char* returns s1
+ */
 char* Mystrncpy(char* s1, const char* s2, size_t n) {
     for (int i = 0; i < n; i++) {
         s1[i] = s2[i];
@@ -18,6 +33,13 @@ char* Mystrncpy(char* s1, const char* s2, size_t n) {
     return s1;
 }
 
+/**
+ * @brief concatinates s2 to s1
+ * 
+ * @param s1 destination
+ * @param s2 to copy
+ * @return char* returns s1
+ */
 char* Mystrcat(char* s1, const char* s2) {
     int i, j;
     for (i = 0; s1[i] != '\0'; i++);
@@ -28,6 +50,15 @@ char* Mystrcat(char* s1, const char* s2) {
     return s1;
 }
 
+/**
+ * @brief concatinates s2 to s1 till n number of characters
+ * 
+ * @param s1 destination
+ * @param s2 to copy
+ * @param n number of characters to concatenate
+ * 
+ * @return char* returns s1
+ */
 char* Mystrncat(char* s1, const char* s2, size_t n) {
     int i, j;
     for (i = 0; s1[i] != '\0'; i++);
@@ -38,6 +69,13 @@ char* Mystrncat(char* s1, const char* s2, size_t n) {
     return s1;
 }
 
+/**
+ * @brief compares the ascii values of s1 with s2
+ * 
+ * @param s1 char array 1
+ * @param s2 char array 2
+ * @return int returns 0 if both are same, returns 1 if s1>s2, returns -1 if s1<s2
+ */
 int Mystrcmp(const char* s1, const char* s2) {
     int i = 0;
     while (1) {
@@ -51,6 +89,14 @@ int Mystrcmp(const char* s1, const char* s2) {
     }
 }
 
+/**
+ * @brief compares the ascii values of s1 with s2 till n number of characters
+ * 
+ * @param s1 char array 1
+ * @param s2 char array 2
+ * @param n number of characters to check
+ * @return int returns 0 if both are same, returns 1 if s1>s2, returns -1 if s1<s2
+ */
 int Mystrncmp(const char* s1, const char* s2, size_t n) {
     int i = 0;
     while (1) {
@@ -64,6 +110,12 @@ int Mystrncmp(const char* s1, const char* s2, size_t n) {
     }
 }
 
+/**
+ * @brief find length of a character array
+ * 
+ * @param s character array
+ * @return size_t returns size of array
+ */
 size_t Mystrlen(const char* s) {
     int i;
     for (i = 0; s[i] != '\0'; i++);
@@ -71,6 +123,13 @@ size_t Mystrlen(const char* s) {
     return i;
 }
 
+/**
+ * @brief tokenizes and returns char array till delimiter
+ * 
+ * @param s1 char array to tokenize
+ * @param s2 delimiter(s)
+ * @return char* returns char array of token
+ */
 char* Mystrtok(char* s1, const char* s2) {
     static char *static_string = s1;
 
